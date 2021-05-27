00f2c0de4464272661b7d599fe01fec3ac393870
noemi86-cpu <noemipatermo@gmail.com>
2021-05-27 16:52:27 +0200

    esercizio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 var nomeUtente = prompt('Inserire nome');
 var cognomeUtente = prompt('Inserire cognome');
 var colorePreferito = prompt('inserire colore preferito');
+document.getElementById("myID").innerHTML = ("nomeUtente + cognomeUtente + colorePreferito + 21");
